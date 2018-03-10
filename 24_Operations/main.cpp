7724752df694c688f1ea7f6fa02513c6e89535d7
Pablo Acevedo <pablo.an@gmail.com>
2018-03-10 23:35:17 +0100

    Added Mathematical Operations

---
+++ 24_Operations/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	//Nombre de la imagen que se va a cargar
+	char NombreImagen1[] = "c://LSI.jpg";
+	char NombreImagen2[] = "c://UC3M.jpg";
+	Mat img1, img2;
+
+	//Cargamos la imagen y se comprueba que lo ha hecho correctamente
+	img1 = imread(NombreImagen1);
+	img2 = imread(NombreImagen2);
+
+	if (!img1.data || !img2.data) {
+		cout << "Error al cargar las imagenes" << endl;
+		exit(1);
+	}
+	imshow("Image1", img1);
+	imshow("Image2", img2);
+	
+	Mat dst;
+	
+	add(img1, img2, dst, noArray(), -1);
+	imshow("ADD", dst);
+    subtract(img1, img2, dst, noArray(), -1);
+    imshow("Subtract", dst);
+    multiply(img1, img2, dst, (1.0), -1);
+    imshow("Multiply", dst);
+    divide(img1, img2, dst, (1.0), -1);
+    imshow("Divide", dst);
+
+	//Esperar a pulsar una tecla
+	cvWaitKey(0);
+	return 0;
+}
